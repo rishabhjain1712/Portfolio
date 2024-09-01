33a74a6337313787da5dc471030c8bbc9898dd51
rishabhjain1712 <rishabh201224@gmail.com>
2024-09-01 19:37:31 +0530

    v1 scroll update

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scrollup.css';
 import { HiOutlineArrowSmUp } from "react-icons/hi";
 
+
 const ScrollUp = () => {
-    window.addEventListener("scroll", function () {
-        const scrollUp = document.querySelector(".scrollup");
-        if(this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    });
-  
+    const [isVisible, setIsVisible] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY >= 560) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        // Cleanup listener on component unmount
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const scrollToTop = (e) => {
+        e.preventDefault(); // Prevent default anchor behavior
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
-    <a href="#" className="scrollup"><HiOutlineArrowSmUp className="scrollup__icon"/></a>
-  );
-}
+        isVisible && (
+            <a 
+                href="#top" 
+                className={`scrollup ${isVisible ? 'show-scroll' : ''}`} 
+                onClick={scrollToTop} 
+                aria-label="Scroll to Top"
+            >
+                <HiOutlineArrowSmUp className="scrollup__icon" />
+            </a>
+        )
+    );
+};
 
 export default ScrollUp;
